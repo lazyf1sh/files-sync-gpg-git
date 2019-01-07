6c22c7722bc5f0c2e8d39ea967d1f86a72a914de
Ivan Kopylov <stealthwar@pm.me>
2019-01-07 15:54:55 +0300

    upg

--- src/master.py
+++ src/master.py
@@ -1,12 +1,21 @@
 import glob
 from src import sync
-from src import Utils
+from src import utils
+from src import encrypt
 
-files_enencrypted = glob.glob("./unencrypted/**", recursive=True)
-struct_md5 = {}
+folder_src = "./unencrypted/**" # use glob format
 
-app_virtual_md5 = sync.buildMd5FilesMapVirtual(files_enencrypted)
-struct_md5 = Utils.readJsonDictFromFile("struct_md5")
-value = sync.findDiffByKey(app_virtual_md5, struct_md5)
+files_unencrypted = glob.glob(folder_src, recursive=True)
 
-print(value)
+current_state = sync.buildMd5FilesMapVirtual(files_unencrypted)
+previous_state = utils.readJsonDictFromFile("previous_state.json")
+
+elements_added = sync.findAddedElementsByKey(current_state, previous_state)
+elements_removed = sync.findRemovedElementsByKey(current_state, previous_state)
+elements_modified = sync.findModifiedFiles(current_state, previous_state)
+
+utils.create_enc_dir_structure(elements_added.keys())
+encrypt.encrypt_files(elements_added.keys())
+
+
+print("end")
