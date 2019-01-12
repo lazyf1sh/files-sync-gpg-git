b70255ca9505984d08ce418955faa9e826dc0e6c
lazyf1sh <f1sh@pm.me>
2019-01-12 09:28:14 +0300

    fixed IDE warnings

--- src/encrypt.py
+++ src/encrypt.py
@@ -1,16 +1,11 @@
-import csv
-import glob
-import hashlib
-import json
 import os
 import subprocess
 import shlex
-import pickle
-import pprint
 
 recipient = "94B31905EEA476BA62B105D0BBDC9AD25A5BEF95!"
 
-def executeCommand(command):
+
+def execute_command(command):
     args = shlex.split(command)
     try:
         proc = subprocess.check_output(args)
@@ -21,17 +16,17 @@ def executeCommand(command):
         print("Output: \n{}\n".format(proc))
 
 
-def gitcommitAndPush(targetFolder):
+def git_commit_and_push(target_folder):
     previous_cwd = os.getcwd()
-    os.chdir(targetFolder)
-    executeCommand('git add *.gpg')
-    executeCommand('git commit -m upd')
+    os.chdir(target_folder)
+    execute_command('git add *.gpg')
+    execute_command('git commit -m upd')
     os.chdir(previous_cwd)
 
 
-def encrypt_files(files_enencrypted, srcFolder, targetFolder):
-    for file in files_enencrypted:
+def encrypt_files(list_files, src_folder, target_folder):
+    for file in list_files:
         if os.path.isfile(file) and not file.lower().endswith('.gpg'):
-            srcFolder = srcFolder.replace("**", "")
-            path_encrypted = targetFolder + os.path.relpath(file, srcFolder)
+            src_folder = src_folder.replace("**", "")
+            path_encrypted = target_folder + os.path.relpath(file, src_folder)
             print(subprocess.check_output(['gpg', '--yes', '--verbose', '--output', path_encrypted + '.gpg', '--encrypt', '--recipient', recipient, file]))
--- src/sync.py
+++ src/sync.py
@@ -2,43 +2,42 @@ import hashlib
 import os
 
 
-def buildMd5FilesMapVirtual(list_files):
+def build_md5_files_map_virtual(list_files):
     result = {}
     for file in list_files:
         if os.path.isfile(file):
             filemd5 = md5(file)
             result[file] = filemd5
         if os.path.isdir(file):
-            md5FromDirPath = md5FromString(file)
-            result[file] = md5FromDirPath
+            md5_from_dir_path = md5_from_string(file)
+            result[file] = md5_from_dir_path
     return result
 
 
-
-
-def findDiffByKey(dict1, dict2):
+def find_diff_by_key(dict1, dict2):
     return {k: dict1[k] for k in set(dict1) - set(dict2)}
 
-def findModifiedFiles(dict1, dict2):
-    sameNames = findIntersectionByKeys(dict1, dict2) # files that have the same names but may be modified
-    same1 = findIntersectionByKeys(dict1, sameNames)
-    same2 = findIntersectionByKeys(dict2, sameNames)
+
+def find_modified_files(dict1, dict2):
+    same_names = find_intersection_by_keys(dict1, dict2)  # files that have the same names but may be modified
+    same1 = find_intersection_by_keys(dict1, same_names)
+    same2 = find_intersection_by_keys(dict2, same_names)
     diff = set(same1.values()) - set(same2.values())
     swapped = {v: k for k, v in dict1.items()}
     result = {swapped[i]: i for i in diff}
     return result
 
 
-
-def findIntersectionByKeys(dict1, dict2):
+def find_intersection_by_keys(dict1, dict2):
     return {k: dict1[k] for k in set(dict1).intersection(set(dict2))}
 
 
-def findAddedElementsByKey(dict1, dict2):
-    return findDiffByKey(dict1, dict2)
+def find_added_elements_by_key(dict1, dict2):
+    return find_diff_by_key(dict1, dict2)
 
-def findRemovedElementsByKey(dict1, dict2):
-    return findDiffByKey(dict2, dict1)
+
+def find_removed_elements_by_key(dict1, dict2):
+    return find_diff_by_key(dict2, dict1)
 
 
 def md5(fname):
@@ -49,5 +48,5 @@ def md5(fname):
     return hash_md5.hexdigest()
 
 
-def md5FromString(string):
+def md5_from_string(string):
     return hashlib.md5(string.encode('utf-8')).hexdigest()
