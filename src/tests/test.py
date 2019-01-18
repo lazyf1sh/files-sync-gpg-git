460e1051237eeb5153fc382666334fd664102261
lazyf1sh <f1sh@pm.me>
2019-01-18 18:31:16 +0300

    added test for md5 from file

--- src/tests/test.py
+++ src/tests/test.py
@@ -1,9 +1,14 @@
 import unittest
-import encrypt
+import glob
+from src import sync
 
 def fun(x):
     return x + 1
 
+
 class MyTest(unittest.TestCase):
     def test(self):
         self.assertEqual(fun(3), 4)
+    def test2(self):
+        file = glob.glob("test_data/file_for_test_md5", recursive=True)
+        self.assertEqual(sync.md5(file[0]), "de9e8b4b671fed9da2518ce488dbc138")
