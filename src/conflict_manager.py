e9056f2d460d657b0f7d8563479fa23d496506d4
lazyf1sh <f1sh@pm.me>
2019-04-14 14:37:53 +0300

    WIP

--- src/conflict_manager.py
+++ src/conflict_manager.py
@@ -1,6 +1,15 @@
 from src import sync, RenameStrategy
 
 def do_resolve(previous_remote_state, current_remote_state, previous_local_state, current_local_state):
+    encrypt_to_right_no_conflicts = {}
+    decrypt_to_left_no_conflicts = {}
+
+    encrypt_to_right_rename_left = {}
+    decrypt_to_left_rename_right = {}
+
+    delete_on_left = {}
+    delete_on_right = {}
+
     remote_elements_added = sync.find_added_elements_by_key(current_remote_state, previous_remote_state)
     remote_elements_removed = sync.find_removed_elements_by_key(current_remote_state, previous_remote_state)
     remote_elements_modified = sync.find_modified_files(current_remote_state, previous_remote_state)
@@ -9,10 +18,9 @@ def do_resolve(previous_remote_state, current_remote_state, previous_local_state
     local_elements_removed = sync.find_removed_elements_by_key(current_local_state, previous_local_state)
     local_elements_modified = sync.find_modified_files(current_local_state, previous_local_state)
 
-    temp_right = {}
-    temp_right.update(remote_elements_added)
-    temp_right.update(remote_elements_modified)
+    sync.find_added_elements_by_key(remote_elements_added, local_elements_added)
+    sync.find_intersection_by_keys(remote_elements_added, local_elements_added)
 
-    copy_to_left = sync.find_added_elements_by_key(temp_right, current_local_state)
+    copy_to_left = sync.find_added_elements_by_key(remote_elements_added, current_local_state)
     copy_to_left_with_timestamp = sync.find_added_elements_by_key(temp_right, local_elements_removed)
 
