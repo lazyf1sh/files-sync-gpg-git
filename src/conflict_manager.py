4a78dd2e4e37613bef5ab97fdfa7ee0c540017e6
lazyf1sh <f1sh@pm.me>
2019-04-14 09:24:49 +0300

    update

--- src/conflict_manager.py
+++ src/conflict_manager.py
@@ -9,9 +9,10 @@ def do_resolve(previous_remote_state, current_remote_state, previous_local_state
     local_elements_removed = sync.find_removed_elements_by_key(current_local_state, previous_local_state)
     local_elements_modified = sync.find_modified_files(current_local_state, previous_local_state)
 
-    temp = {}
-    temp.update(remote_elements_added)
-    temp.update(remote_elements_modified)
+    temp_right = {}
+    temp_right.update(remote_elements_added)
+    temp_right.update(remote_elements_modified)
 
-    copy_to_left = sync.find_added_elements_by_key(temp, current_local_state)
+    copy_to_left = sync.find_added_elements_by_key(temp_right, current_local_state)
+    copy_to_left_with_timestamp = sync.find_added_elements_by_key(temp_right, local_elements_removed)
 
