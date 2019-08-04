93699ab56f062c917603cbcae905c9db1ebe863c
lazyf1sh <f1sh@pm.me>
2019-08-04 12:36:14 +0300

    updated logging; added option to disable gpg verbose mode;

--- src/script/gpg.py
+++ src/script/gpg.py
@@ -21,10 +21,15 @@ def decrypt_single_file(source_path, target_path):
     if os.path.isfile(source_path):
         if source_path.lower().endswith('.gpg'):
             try:
-                output = subprocess.check_output(['gpg', '--yes', '--output', target_path, '--verbose', '--decrypt', source_path])
+                args = ['gpg', '--yes', '--output', target_path]
+                args.append('--verbose')
+                args.append('--decrypt')
+                args.append(source_path)
+
+                output = subprocess.check_output(args)
                 logger.debug(output)
             except subprocess.CalledProcessError as e:
-                logger.critical("Error decrypting " + source_path + " " + str(e.output))
+                logger.critical("Error decrypting %s Exception: %s", source_path, e)
         else:
             logger.error("The file have no gpg extension. that is very strange situation, because in repo must be only .gpg-files: %s", source_path)
     else:
@@ -50,7 +55,15 @@ def decrypt_single_file_inmemory(source_path, working_dir):
 def encrypt_single_file(src_path, target_path, working_dir):
     if os.path.isfile(src_path):
         logging.info("encrypting file: %s", src_path)
-        args = ['gpg', '--yes', '--verbose', '--output', target_path, '--encrypt', '--recipient', recipient, src_path]
+        args = ['gpg', '--yes']
+        if gpg_verbose:
+            args.append('--verbose')
+        args.append('--output')
+        args.append(target_path)
+        args.append('--encrypt')
+        args.append('--recipient')
+        args.append(recipient)
+        args.append(src_path)
 
         output = utils.execute_command_args_bytes(args, working_dir)
         logger.info("encrypted file: %s", output)
