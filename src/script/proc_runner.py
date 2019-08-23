213737448ea9f89ae02750024d99b021b40484ff
lazyf1sh <f1sh@pm.me>
2019-08-23 14:41:23 +0300

    moved process running-specific functions to separate file

--- src/script/proc_runner.py
+++ src/script/proc_runner.py
@@ -1,6 +1,8 @@
 import logging
 import os
+import shlex
 import subprocess
+import sys
 from subprocess import Popen
 
 logger = logging.getLogger(__name__)
@@ -24,3 +26,48 @@ def run_piped(working_dir, args1, args2):
         raise exc
     finally:
         os.chdir(previous_working_dir)
+
+
+def execute_command_args_bytes(args, working_dir):
+    return execute_command_internal(args, working_dir)
+
+
+def execute_command_internal(args, working_dir):
+    """
+
+    :param args:
+    :param working_dir:
+    :return: bytes
+    """
+    logger.debug("executing command: %s | working dir: %s", args, working_dir)
+    previous_working_dir = os.getcwd()
+    os.chdir(working_dir)
+    try:
+        proc = subprocess.check_output(args)
+        # logger.debug("Command executed: %s\nExec result: %s\n", args, proc)
+        return proc
+    except subprocess.CalledProcessError as exc:
+        logger.critical("Critical error: Code: %s", exc.returncode)
+        logger.critical("Critical error. Exception: %s", str(exc))
+        raise exc
+    finally:
+        os.chdir(previous_working_dir)
+
+
+def execute_command_args(args, working_dir) -> str:
+    my_bytes = execute_command_internal(args, working_dir)
+    try:
+        return my_bytes.decode("UTF-8")
+    except Exception as e:
+        logger.warning(e)
+        return str(my_bytes)
+
+
+def execute_command_string(command, working_dir) -> str:
+    args = shlex.split(command, posix="win" not in sys.platform)
+    bytes1 = execute_command_internal(args, working_dir)
+    try:
+        return bytes1.decode("UTF-8")
+    except Exception as e:
+        logger.error(e)
+        raise Exception("Error executing command")
--- src/script/utils.py
+++ src/script/utils.py
@@ -110,49 +110,7 @@ def remove_files(paths, src_folder, target_folder):
             os.remove(encrypted_path)
 
 
-def execute_command_args_bytes(args, working_dir):
-    return execute_command_internal(args, working_dir)
 
-
-def execute_command_internal(args, working_dir):
-    """
-
-    :param args:
-    :param working_dir:
-    :return: bytes
-    """
-    logger.debug("executing command: %s | working dir: %s", args, working_dir)
-    previous_working_dir = os.getcwd()
-    os.chdir(working_dir)
-    try:
-        proc = subprocess.check_output(args)
-        # logger.debug("Command executed: %s\nExec result: %s\n", args, proc)
-        return proc
-    except subprocess.CalledProcessError as exc:
-        logger.critical("Critical error: Code: %s", exc.returncode)
-        logger.critical("Critical error. Exception: %s", str(exc))
-        raise exc
-    finally:
-        os.chdir(previous_working_dir)
-
-
-def execute_command_args(args, working_dir) -> str:
-    my_bytes = execute_command_internal(args, working_dir)
-    try:
-        return my_bytes.decode("UTF-8")
-    except Exception as e:
-        logger.warning(e)
-        return str(my_bytes)
-
-
-def execute_command_string(command, working_dir) -> str:
-    args = shlex.split(command, posix="win" not in sys.platform)
-    bytes1 = execute_command_internal(args, working_dir)
-    try:
-        return bytes1.decode("UTF-8")
-    except Exception as e:
-        logger.error(e)
-        raise Exception("Error executing command")
 
 
 def get_current_unix_ts():
